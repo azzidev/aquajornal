e3e8c944b523216593ccd3d5f8b88b33f66e0d80
Matheus Azzi <mazzic.dev@gmail.com>
2023-05-19 10:28:46 -0300

    set partials

---
+++ partials/header.php
@@ -0,0 +1,108 @@
+<!-- Header Start -->
+<header>
+    <div class="header-area">
+        <div class="main-header ">
+            <div class="header-top black-bg d-none d-sm-block">
+                <div class="container">
+                    <div class="col-xl-12">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="header-info-left">
+                                <ul>     
+                                    <li class="title"><span class="flaticon-energy"></span> TECNOLOGIA</li>
+                                    <li>Presidente Lula decide implantar programação nas escolas estaduais</li>
+                                </ul>
+                            </div>
+                            <div class="header-info-right">
+                                <ul class="header-date">
+                                    <li><span class="flaticon-calendar"></span>19/05/2023 às 15:24</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-mid gray-bg">
+                <div class="container">
+                    <div class="row d-flex align-items-center">
+                        <!-- Logo -->
+                        <div class="col-xl-3 col-lg-3 col-md-3 d-none d-md-block">
+                            <div class="logo">
+                                <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="col-xl-9 col-lg-9 col-md-9">
+                            <div class="header-banner f-right ">
+                                <img src="assets/img/gallery/header_card.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-8 col-lg-8 col-md-12 header-flex">
+                            <!-- sticky -->
+                            <div class="sticky-logo">
+                                <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                            <!-- Main-menu -->
+                            <div class="main-menu d-none d-md-block">
+                                <nav>                  
+                                    <ul id="navigation">
+                                        <li><a href="index.html">Início</a></li>
+                                        <li><a href="weather.html">Tempo</a></li>
+                                        <li><a href="#">Categorias</a>
+                                            <div class="row submenu">
+                                                <div class="col-md-4">
+                                                    <a href="tech.html">Tecnologia</a>
+                                                    <a href="edu.html">Educação</a>
+                                                    <a href="sports.html">Esporte</a>
+                                                    <a href="games.html">Jogos</a>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <a href="tech.html">Tecnologia</a>
+                                                    <a href="edu.html">Educação</a>
+                                                    <a href="sports.html">Esporte</a>
+                                                    <a href="games.html">Jogos</a>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <a href="tech.html">Tecnologia</a>
+                                                    <a href="edu.html">Educação</a>
+                                                    <a href="sports.html">Esporte</a>
+                                                    <a href="games.html">Jogos</a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li><a href="latest-news.html">Artigos recentes</a></li>
+                                        <li><a href="contact.html">Colaborar</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>             
+                        <div class="col-xl-4 col-lg-4 col-md-4">
+                            <div class="header-right f-right d-none d-lg-block">
+                                <!-- Heder social -->
+                                <ul class="header-social">    
+                                    <li><a href="https://www.fb.com/sai4ull"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li> <a href="#"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                                <!-- Search Nav -->
+                                <div class="nav-search search-switch">
+                                    <i class="fa fa-search"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-md-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header End -->

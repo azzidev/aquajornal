69d61b5aace2fe1ecc31d2777308231d1522a3b3
Matheus Azzi <mazzic.dev@gmail.com>
2023-05-22 12:27:30 -0300

    fixing server errors in news page and set a new visual identit

--- modules/functions.php
+++ modules/functions.php
@@ -1,6 +1,6 @@
 <?php
     function clearString($string){
-        $a = array("aquajornal","aqua","news","data","date","bcc","root","+","--","&","/","hash","user","id","mysql","insert","select","create","alter","delete","from","*","1=1",'""',"''","drop","DROP","table","TABLE");
+        $a = array("aquajornal","aqua","news","data","date","bcc","root","+","--","&","/","hash","mysql","insert","select","create","alter","delete","from","*","1=1",'""',"''","drop","DROP","table","TABLE");
         return str_replace($a, '',$string);
     }
 ?>
